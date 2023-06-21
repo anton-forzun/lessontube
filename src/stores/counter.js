e69690c5bc505b1c3eebb4224ea3cbc9e21694d7
Anton Forzun <antonforzun@gmail.com>
2023-06-22 00:33:39 +0300

    first commit

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,35 +1,10 @@
 import { defineStore } from "pinia"
-// const lessons = import ("../apiVideo")
+import lessons from "../apiVideo"
 
 export const useCounterStore = defineStore("counter", {
   state: () => {
     return { 
-        videos: [
-            {
-              title: 'Lesson 1',
-              subtitle: 'Subtitle 1',
-              video_url: 'src/assets/lesson1.mp4',
-              thumbnail: "//i.ytimg.com/vi/KB4_WIPE7vo/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCCxXm7aoPShOwON74nhMlGYMUkHw",
-              video_time: 11,
-              description: 'Lesson 1 description'
-            },
-            {
-              title: 'Lesson 2',
-              subtitle: 'Subtitle 2',
-              video_url: 'src/assets/lesson2.mp4',
-              video_time: 16,
-              thumbnail: "//i.ytimg.com/vi/jn9mHzXJIV0/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAvJvk4k_UNB9nst4pFP-txM1TLZA",
-              description: 'Lesson 2 description'
-            },
-            {
-              title: 'Lesson 3',
-              subtitle: 'Subtitle 3',
-              video_url: 'src/assets/lesson3.mp4',
-              video_time: 11,
-              thumbnail: "//i.ytimg.com/vi/UIwdCN4dV6w/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDhlan32jHSvicGZezDFPjAOdXGUA",
-              description: 'Lesson 3 description'
-            },
-          ]
+        videos: lessons
     };
   },
   actions: {
